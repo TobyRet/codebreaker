cd26953edc28c23c08373085cd50b66d134d16a1
Toby Retallick <toby.retallick@gmail.com>
2014-11-11 23:04:52 +0000

    Codebreaker prints welcome message and asks for first guess

--- spec/codebreaker/game_spec.rb
+++ spec/codebreaker/game_spec.rb
@@ -2,9 +2,24 @@ require 'spec_helper'
 
 module Codebreaker
 	describe Game do 
-		desribe "#start" do 
-			it "sends a welcome message"
-			it "prompts for the first guess"
+		describe "#start" do 
+			let(:output) { double('output').as_null_object } 
+
+			it "sends a welcome message" do
+				game = Game.new(output)
+
+				expect(output).to receive(:puts).with('Welcome to Codebreaker!')
+
+				game.start
+			end
+
+			it "prompts for the first guess" do 
+				game = Game.new(output)
+
+				expect(output).to receive(:puts).with('Enter guess:')
+
+				game.start
+			end
 		end
 	end
 end
